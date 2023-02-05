68edb294fde5979ab0639e2a0a0535be81df38bc
Manidoux41 <lherissondu41@gmail.com>
2023-02-05 18:47:35 +0100

    test deploy on vercel

--- back/server.js
+++ back/server.js
@@ -1,13 +1,26 @@
 const express = require("express");
+const path = require('path')
 
 const menuRouter = require("./routes/menuRouter");
 
 const app = express();
+const port = process.env.SERVER_PORT || 5001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", menuRouter);
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-const port = process.env.SERVER_PORT || 5001;
+app.get("*", function(_, res) {
+    res.sendFile(
+        path.join(__dirname, "../frontend/build/index.html"),
+        function (err) {
+            if(err) {
+                res.status(500).send(err)
+            }
+        }
+    )
+})
+
 app.listen(port, () => console.log(`Listening on port: ${port}`))
