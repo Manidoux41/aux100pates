bb4c3d361a30971f88e9375cee14cdcff80d5763
Manidoux41 <lherissondu41@gmail.com>
2023-02-05 19:30:55 +0100

    add new server

--- back/server.js
+++ back/server.js
@@ -1,13 +1,14 @@
 const express = require("express");
-
+const cors = require("cors");
 const menuRouter = require("./routes/menuRouter");
 
 const app = express();
 const port = process.env.SERVER_PORT || 5001;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", menuRouter);
 
-app.listen(port, () => console.log(`Listening on port: ${port}`))
+module.exports = app;
