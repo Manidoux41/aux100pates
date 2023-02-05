eb4b29a0abc47b8b2e8d5d45f80782f749e75a2a
Manidoux41 <lherissondu41@gmail.com>
2023-02-06 00:29:50 +0100

    test affichge des menus

--- front/src/components/MenuBoard.jsx
+++ front/src/components/MenuBoard.jsx
@@ -7,7 +7,14 @@ const MenuBoard = () => {
         <div className="menu-board">
           <div className="menu-content">
             <h1>Nos menus</h1>
-
+            <ul>
+              <li>Reine</li>
+              <li>4 saisons</li>
+              <li>Jardinière</li>
+              <li>Végétarienne...</li>
+              <li></li>
+            </ul>
+            <h2>Le reste des menus seront intégré directement via la backend de l'application...</h2>
           </div>
         </div>
       </div>
